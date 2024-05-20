abba836a4afc7661397ce3f6927fa4b937c9054d
Tyler-Bibus <tylerbibus@hotmail.com>
2024-05-20 14:48:26 -0500

    App runs and tracks number of sets TODO: add reps per set for volume Then start using OOP to create classes for movements that contain sets and muscle distribution.

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -61,6 +61,8 @@ dependencies {
     implementation("androidx.compose.material3:material3")
     //implementation("com.google.android.gms:play-services-ads:22.6.0")
     implementation("androidx.constraintlayout:constraintlayout:2.2.0-alpha13")
+    implementation("androidx.recyclerview:recyclerview:1.3.2")
+    //implementation("com.google.android.gms:play-services-ads:23.1.0")
     testImplementation("junit:junit:4.13.2")
     androidTestImplementation("androidx.test.ext:junit:1.1.5")
     androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
--- app/src/main/java/com/northstarfit/MainActivity.kt
+++ app/src/main/java/com/northstarfit/MainActivity.kt
@@ -1,21 +1,51 @@
 package com.northstarfit
 
 import android.os.Bundle
+import android.util.Log
+import android.widget.Button
+import android.widget.EditText
+import android.widget.Spinner
+import android.widget.TextView
 import androidx.activity.ComponentActivity
-import androidx.activity.compose.setContent
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.material3.MaterialTheme
-import androidx.compose.material3.Surface
-import androidx.compose.material3.Text
-import androidx.compose.runtime.Composable
-import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
-import com.northstarfit.ui.theme.NorthStarFitTheme
+
 
 
 class MainActivity : ComponentActivity() {
+    private lateinit var btSubmit: Button
+    private lateinit var tvTotalSets: TextView
+    private lateinit var spExcercise: Spinner
+    private lateinit var etWeight: EditText
+    private lateinit var tvVolume: TextView
+    private var setsCompleted = 0
+    private var totalVolume = 0.0
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.log_workout)
+        btSubmit = findViewById(R.id.btSubmitExcercise)
+        tvTotalSets = findViewById(R.id.tvSetsCompleted)
+        etWeight = findViewById(R.id.etWeight)
+        tvVolume = findViewById(R.id.tvTotalVolume)
+        setWeights()
+
+
+        btSubmit.setOnClickListener{
+            Log.d("MainActivity", "Workout Submitted")
+            setWeights()
+        }
+
     }
+
+    private fun setWeights() {
+        if (etWeight.text.isEmpty()){
+            tvTotalSets.text = "Sets: " + setsCompleted.toString()
+            tvVolume.text = "Volume: " + totalVolume.toString()
+            return
+        }
+        setsCompleted++
+        totalVolume += etWeight.text.toString().toDouble()
+        tvTotalSets.text = "Sets: " + setsCompleted.toString()
+        tvVolume.text = "Volume: " + "%.2f".format(totalVolume)
+    }
+
+
 }
